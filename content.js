ec183b47eb7fb9a70946d9f77c422d170bfb898c
MG2003 <max.guo2014@gmail.com>
2023-01-29 09:15:13 -0500

    Fire overlay

--- content.js
+++ content.js
@@ -1,8 +1,27 @@
-const site = window.location.hostname;
-alert("Injection works to " + site);
+const imgurlinks = {
+    fire_1: "https://i.imgur.com/kW4VZMI.gif",
+    fire_2: "https://i.imgur.com/k6MYOsI.gif",
+    fire_3: "https://i.imgur.com/P80fvF9.gif",
+    fire_4: "https://i.imgur.com/AV52THF.gif",
+    fire_5: "https://i.imgur.com/SvOld88.gif",
+    fire_6: "https://i.imgur.com/K0SFGJz.gif",
+    fire_7: "https://i.imgur.com/wbioXIu.gif",
+    fire_8: "https://i.imgur.com/GkS4Hnw.gif",
+    fire_9: "https://i.imgur.com/GmiANJD.gif",
+    fire_0: "https://i.imgur.com/Yf4iFR3.png",
+    fire_10: "https://i.imgur.com/4XsxdJg.gif",
+    bg: "https://i.imgur.com/5zYjMqH.png"
+};
 
+
+const site = window.location.hostname;
 var fire = document.createElement("img");
-fire.src = "https://cs.brown.edu/courses/csci0190/2022/shriram.jpg"
+fire.src = imgurlinks.fire_9;
+fire.style.position = "fixed";
+fire.style.bottom = "10px";
+fire.style.left = 0;
+fire.style.zIndex = 9999;
+fire.width = 100;
+
 var src = document.getElementById("body");
 document.body.append(fire);
-alert("done");
--- overlay.js
+++ overlay.js
@@ -5,7 +5,7 @@ displayBackground();
 
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse){            
-        img = document.createElement("fire_"+request.content.toString()+".png");
+        img = document.createElement("Fire"+request.content.toString()+".png");
         img.src = src;
         img.width = 48;
         img.height = 64;
@@ -17,6 +17,7 @@ function displayBackground(src) { //Display the background image
     img.src = src;
     img.width = 48;
     img.height = 64;
+    img.style.position = fixed;
     document.body.appendChild(img);
    }
    
