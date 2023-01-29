d5ab46139d3a932c4f2579892945045dc9d5db9c
MG2003 <max.guo2014@gmail.com>
2023-01-29 09:24:22 -0500

    changed imgurlinks to a list

--- content.js
+++ content.js
@@ -1,22 +1,20 @@
-const imgurlinks = {
-    fire_1: "https://i.imgur.com/kW4VZMI.gif",
-    fire_2: "https://i.imgur.com/k6MYOsI.gif",
-    fire_3: "https://i.imgur.com/P80fvF9.gif",
-    fire_4: "https://i.imgur.com/AV52THF.gif",
-    fire_5: "https://i.imgur.com/SvOld88.gif",
-    fire_6: "https://i.imgur.com/K0SFGJz.gif",
-    fire_7: "https://i.imgur.com/wbioXIu.gif",
-    fire_8: "https://i.imgur.com/GkS4Hnw.gif",
-    fire_9: "https://i.imgur.com/GmiANJD.gif",
-    fire_0: "https://i.imgur.com/Yf4iFR3.png",
-    fire_10: "https://i.imgur.com/4XsxdJg.gif",
-    bg: "https://i.imgur.com/5zYjMqH.png"
-};
+
+const imgurlist = ["https://i.imgur.com/Yf4iFR3.png", "https://i.imgur.com/kW4VZMI.gif", "https://i.imgur.com/k6MYOsI.gif", "https://i.imgur.com/P80fvF9.gif", 
+"https://i.imgur.com/AV52THF.gif", "https://i.imgur.com/SvOld88.gif", "https://i.imgur.com/K0SFGJz.gif",
+"https://i.imgur.com/wbioXIu.gif", "https://i.imgur.com/GkS4Hnw.gif", "https://i.imgur.com/GmiANJD.gif", "https://i.imgur.com/4XsxdJg.gif"
+];
+function changeFireLevel(fireStrength){
+
+    currFire = imgurlist[fireStrength];
+
+}
+
+var currFire = "https://i.imgur.com/Yf4iFR3.png";
 
 
 const site = window.location.hostname;
 var fire = document.createElement("img");
-fire.src = imgurlinks.fire_9;
+fire.src = currFire;
 fire.style.position = "fixed";
 fire.style.bottom = "10px";
 fire.style.left = 0;
