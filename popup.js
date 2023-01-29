ba07cbd9225dddda97ec52ee412ee58b1d4af0d2
Equite774 <chai.harsha7@gmail.com>
2023-01-29 11:10:15 -0500

    changed html

--- popup.js
+++ popup.js
@@ -1,1 +1,4 @@
-console.log("hi");
+
+//var time = currentSeconds + distractedSeconds / 3600+ ":"+ (currentSeconds + distractedSeconds % 3600) / 60+ ":"+ ((currentSeconds + distractedSeconds % 3600) / 60) % 60;
+document.getElementById("timer").textContent = "hi";
+
