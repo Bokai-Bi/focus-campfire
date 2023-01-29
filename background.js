f5b4d2e887dc3b0fe8c39fd182a052064e2a16ef
Equite774 <chai.harsha7@gmail.com>
2023-01-28 20:24:51 -0500

    .

--- background.js
+++ background.js
@@ -134,6 +134,6 @@ function updateFireStrength(){
 
 function overlay(){
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.executeScript(tabs[0].id,{file:"js/overlay.js"});
+        chrome.tabs.executeScript(tabs[0].id,{file:"js/popup.js"});
       });
 }
